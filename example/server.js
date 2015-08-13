20eabffee9bd97b9021b04d1d08ae9b3d2d95b63
Ashley Davis <ashley@codecapers.com.au>
2015-08-13 11:57:55 +1000

    Added an example of use.

---
+++ example/server.js
@@ -0,0 +1,14 @@
+
+var structuredLog = require('structured-log-dev');
+var consoleSink = require('structured-log-dev/console-sink');
+var httpSink = require('../structured-log-http-sink');
+
+var log = structuredLog.configure()
+	.writeTo(httpSink({ url: 'http://localhost:3000/log' }))
+    .create();
+
+log.info('Hello this is some information.');
+
+log.warn('This is a warning.');
+
+log.error('This is an error.');
